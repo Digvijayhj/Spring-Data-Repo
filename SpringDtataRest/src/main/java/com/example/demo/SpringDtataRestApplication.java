04cbb0b6204df0f568f751100c8426f07fecc94a
Digvijayhj <digvijayhj@gmail.com>
2021-02-01 21:02:26 +0530

    First Commit

---
+++ SpringDtataRest/src/main/java/com/example/demo/SpringDtataRestApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDtataRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringDtataRestApplication.class, args);
+	}
+
+}
